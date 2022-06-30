090d0e409ea391f67bd7ea904fc1b9aa5deb2e03
Mohammad Amin Mollazadeh <madamin@pm.me>
2022-06-30 21:28:18 +0430

    feat(SettingsService): save/load to shared storage

--- lib/src/settings/settings_service.dart
+++ lib/src/settings/settings_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// A service that stores and retrieves user settings.
 ///
@@ -6,12 +7,23 @@ import 'package:flutter/material.dart';
 /// persist the user settings locally, use the shared_preferences package. If
 /// you'd like to store settings on a web server, use the http package.
 class SettingsService {
+  SharedPreferences? _sharedPreferences;
+
+  SettingsService();
+
   /// Loads the User's preferred ThemeMode from local or remote storage.
-  Future<ThemeMode> themeMode() async => ThemeMode.system;
+  Future<ThemeMode> themeMode() async {
+    _sharedPreferences ??= await SharedPreferences.getInstance();
+    final themeModeString = _sharedPreferences!.getString('themeMode');
+    if (themeModeString == null) {
+      return ThemeMode.system;
+    }
+    return ThemeMode.values[int.parse(themeModeString)];
+  }
 
   /// Persists the user's preferred ThemeMode to local or remote storage.
   Future<void> updateThemeMode(ThemeMode theme) async {
-    // Use the shared_preferences package to persist settings locally or the
-    // http package to persist settings over the network.
+    _sharedPreferences ??= await SharedPreferences.getInstance();
+    _sharedPreferences!.setString('themeMode', theme.index.toString());
   }
 }
